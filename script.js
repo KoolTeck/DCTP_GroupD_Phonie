7c1f48195d019c908b9ffe522c43163c17068c97
Adetunji Adebowale <bowaleadetunji@gmail.com>
2023-10-11 22:49:34 +0100

    added some refac

--- script.js
+++ script.js
@@ -2,10 +2,11 @@ document.getElementById("phoneNumber").addEventListener("input", function () {
   const phoneNumber = this.value;
   const errorDiv = document.getElementById("error-message");
 
-  if (/^\d+$/.test(phoneNumber) && phoneNumber.length <= 11) {
-    errorDiv.textContent = "";
+  if (!/^\d+$/.test(phoneNumber)) {
+    errorDiv.textContent = "Phone number must only contain digits";
+  } else if (phoneNumber.length > 11) {
+    errorDiv.textContent = " Phone number must  be 11 characters or less.";
   } else {
-    errorDiv.textContent =
-      "Phone number must only contain digits and be 11 characters or less.";
+    errorDiv.textContent = "";
   }
 });
